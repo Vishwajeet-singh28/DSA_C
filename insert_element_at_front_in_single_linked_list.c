153c2225dc310229b89c9c70937731759b90119d
VISHWAJEET SINGH <vishwajeetsinghrajput75@gmail.com>
2026-09-01 23:22:14 +0530

    Add function to insert element at front of linked list
    Implement insertion of a new node at the front of a singly linked list and display the list before and after the insertion.

---
+++ insert_element_at_front_in_single_linked_list.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *head=NULL,*temp,*newnode;
+    int n,i,value;
+    
+    
+    printf("enter no. of nodes: ");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        newnode=(struct node*)malloc(sizeof(struct node));
+        
+        printf("enter data %d: ",i+1);
+        scanf("%d",&newnode->data);
+        
+        newnode->next=NULL;
+        
+        if(head==NULL){
+            head=newnode;
+            temp=newnode;
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+        
+    }
+    
+    printf("linked list before insertion: \n");
+    
+    temp=head;
+    
+    while(temp!=NULL){
+        printf("%d ->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+    
+    
+    printf("\nenter value you want to enter: ");
+    scanf("%d",&value);
+    
+    newnode=(struct node*)malloc(sizeof(struct node));
+    
+    newnode->data=value;
+    newnode->next=head;
+    head=newnode;
+    
+    printf("\nlinked list after insertion: \n");
+    
+    temp=head;
+    
+    while(temp!=NULL){
+        printf("%d ->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+    return 0;
+}
